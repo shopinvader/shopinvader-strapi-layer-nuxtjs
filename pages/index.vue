415e343faf1f7419fc39672f57bc1043c242cdc0
thibault <thibault.rey@akretion.com>
2023-06-19 16:58:16 +0200

    [IMP] Add playground

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="page && page?.content">
+    <cms-dynamic-zone :dynamicZone="page?.content" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { Page } from "~/models/cms/Page";
+const route = useRoute();
+const slugs: string[] = (route.params.slugs as string[]) || [];
+const path: string | null = (slugs.join("/") as string) || null;
+const { data: page, error } = await useAsyncData(
+  "page",
+  async () => {
+    const { findPage } = useCMS();
+    const page: Page | null =
+      (await findPage({ filters: { handle: "home" } })) || null;
+    return page;
+  },
+  { watch: [path] }
+);
+const test = async () => {
+  console.log("test");
+  const { find } = useStrapi();
+  const a = await find("pages", "home");
+  return a;
+};
+if (page?.value) {
+  const { seo } = page.value;
+  useHead({
+    title: seo?.metaTitle || "Home",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: seo?.metaDescription || "",
+      },
+    ],
+  });
+}
+</script>
